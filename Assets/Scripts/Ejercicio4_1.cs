e2187425c4adf1f9b2d4e0426d9cc33dbd99c142
anyuegonzalez <anyuee07@gmail.com>
2024-09-16 18:16:25 +0200

    hjgdj

--- Assets/Scripts/Ejercicio4_1.cs
+++ Assets/Scripts/Ejercicio4_1.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Ejercicio4_1 : MonoBehaviour
 {
-    // se me dan mal las mates complejas
+    [SerializeField] float areaCirculo;
+    [SerializeField] float areaCuadrado;
+    [SerializeField] float areaTriangulo;
+    [SerializeField] int circulo, triangulo, cuadrado;
+    private float radioAlCuadrado,baseT = 17.8f,altura = 26.2f,lado,pi = 3.141592f;
     void Start()
     {
-        
+        AreaCirculo();
+        Debug.Log("El area del circulo es: " + areaCirculo);
+        AreaTriangulo();
+        Debug.Log("El area del Triangulo es: " + areaTriangulo);
     }
 
     
     void Update()
     {
         
+    }
+    void AreaCirculo()
+    {
+        areaCirculo = pi * radioAlCuadrado;
+    }
+    void AreaTriangulo()
+    {
+
     }
 }
