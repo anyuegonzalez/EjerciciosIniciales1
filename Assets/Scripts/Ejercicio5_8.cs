a9ff4f7bdf331af6a8fb1ffe3581951fe8916fd9
anyuegonzalez <anyuee07@gmail.com>
2024-09-21 20:00:42 +0200

    fw

--- Assets/Scripts/Ejercicio5_8.cs
+++ Assets/Scripts/Ejercicio5_8.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ejercicio5_8 : MonoBehaviour
-{    
+{
+    // no se como hacer que el programa me de 3 horas aleatorias
+    [SerializeField] int horas;
+    [SerializeField] int minutos;
+    [SerializeField] int segundos;
     void Start()
     {
-        
+        if ((horas >= 0 && horas < 24) && (minutos >= 0 && minutos < 60) && (segundos >= 0 && segundos < 3600))
+        {
+            Debug.Log("La hora es válida: ");
+        }
+        else 
+        {
+            Debug.Log("La hora es inválida");
+        }
     }
 
    
     void Update()
     {
         
+    }
+    void CrearNumero()
+    {
+
     }
 }
