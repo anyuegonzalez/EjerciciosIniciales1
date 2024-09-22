c59c82f68e505163dcd9570c9688209d970f4003
anyuegonzalez <anyuee07@gmail.com>
2024-09-22 16:33:05 +0200

    hjtghj

--- Assets/Scripts/Ejercicio_7/Ejercicio7_13.cs
+++ Assets/Scripts/Ejercicio_7/Ejercicio7_13.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class Ejercicio7_13 : MonoBehaviour
 {
+    [SerializeField] int num = 17;
+    bool esPrimo = true;
    
     void Start()
     {
-        
+      
+        if (esPrimo)
+        {
+            Debug.Log("El numero es primo");
+        }
+        else
+        {
+            Debug.Log("No es primo");
+        }
     }
-
+    /*
+    public bool EsPrimo()
+    {
+        if (num <= 2)
+        {
+            return false;
+        }
+        for (int i = 0; i < num; i++)
+        {
+            if(num % i == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+    }
+    */
    
 }
