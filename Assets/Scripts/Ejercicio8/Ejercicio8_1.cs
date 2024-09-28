4992ae3876f5ff8c96aedc1e8993de37047c4fab
anyuegonzalez <anyuee07@gmail.com>
2024-09-28 20:41:59 +0200

    cxdcs

---
+++ Assets/Scripts/Ejercicio8/Ejercicio8_1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio8_1 : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+   
+}
